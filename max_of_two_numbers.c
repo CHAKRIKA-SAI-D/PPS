c61e190259d33551de96b573b5d8e308ecdd814e
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-01-28 20:02:15 +0530

    Update and rename ternary_condition.c to max_of_two_numbers.c

---
+++ max_of_two_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+int a,b,max;
+printf("enter the numbers : ");
+scanf("%d%d%d",&a,&b);
+max =(a>b)?a:b;
+printf("max is %d",max); 
+return 0;
+}
+
+enter the numbers : 10
+19
+max is 19
+
