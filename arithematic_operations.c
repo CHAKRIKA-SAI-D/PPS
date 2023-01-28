9b6922aa342f0c051ffb2c513e664bdbda0eebb7
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-01-28 18:46:11 +0530

    Update arithematic_operations.c

--- arithematic_operations.c
+++ arithematic_operations.c
@@ -9,4 +9,13 @@ printf("the multiplication of two numbers is %d",num1*num2);
 printf("the division of two numbers is %d",num1/num2);
 printf("the remainder when divided the two numbers is %d",num1%num2);
 return;
-} 
+}
+
+# output :
+enter two numbers: 2
+2
+The addition of two numbers is 4
+  the difference of two numbers is 0
+  the multiplication of two numbers is 4
+  the division of two numbers is 1
+  the remainder when divided the two numbers is 0
