6c987118074ba85876d82659f20e733c63d132d5
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-01-28 19:55:18 +0530

    Update simple_and_compound_interest.c

--- simple_and_compound_interest.c
+++ simple_and_compound_interest.c
@@ -11,3 +11,9 @@ compound_interest =p*pow((1+r/100),t);
 printf("the simple interest is %lf and compound  interest is %lf",simple_interest,compound_interest);
 return 0;
 }
+
+#output : 
+enter the principle amount(p) : 10
+time and rate in percent2
+100
+the simple interest is 20.000000 and compound  interest is 40.000000
