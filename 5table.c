0340a5eadf4bbaccfcef54c31284fbbfc4e64185
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-01-28 18:40:13 +0530

    Update 5table.c

--- 5table.c
+++ 5table.c
@@ -1,3 +1,4 @@
+code :
 #include <stdio.h>
 int main(){
 int num,i=1;
@@ -12,3 +13,17 @@ i++;
 }
 return 0;
 }
+
+# output : 
+enter the number : 5
+enter last number : 10
+5 x 1 =5
+5 x 2 =10
+5 x 3 =15
+5 x 4 =20
+5 x 5 =25
+5 x 6 =30
+5 x 7 =35
+5 x 8 =40
+5 x 9 =45
+5 x 10 =50
