ff37250fa33d2df396c6bd40dd65bd147fac7298
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-01-30 15:09:01 +0530

    lab_cycle_3

---
+++ pyramid_of_stars.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+int n;
+printf("enter the number of rows : ");
+scanf("%d",&n);
+for(int i=1;i<=n;i++){
+for(int j=1;j<=i;j++){
+printf("*");
+}
+printf("\n");
+}
+return 0;
+  
+
+output :enter the number of rows : 5
+  *
+  **
+  ***
+  ****
+  *****
