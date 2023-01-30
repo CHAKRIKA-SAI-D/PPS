d5aef652f59be28f4b0c6af8a3ffe297779a88da
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-01-30 18:20:07 +0530

    lab_cycle_2

---
+++ fact_with_reccursion.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int fact(int n);
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    printf("Factorial of %d = %d", n, fact(n));
+    return 0;
+}
+
+ int fact(int n) {
+    if (n>=1)
+        return n*fact(n-1);
+    else
+        return 1;
+}
+
+
+output : 
+Enter a positive integer: 5
+  Factorial of 5 = 120
